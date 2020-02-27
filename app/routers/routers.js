e58a89a0c02ababf031e86d220459e99fcba4a77
fabio.sousa <fabiossilva00@gmail.com>
2020-02-27 16:59:15 -0300

    Alterações: - Atualização do react-native para a versão 0.61.5; - Atualizado libs de funcionamento e atualizado código; '

--- app/routers/routers.js
+++ app/routers/routers.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 import Main from '../src/views/Main'
 import DetalhesPokemon from '../src/views/DetalhesPokemon'
